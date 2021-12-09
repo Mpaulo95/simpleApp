0753d03c35716f1377486201b32ab63e8d88223c
Elie Elisee Paul <marcoht023@hotmail.com>
2021-12-09 02:21:43 -0500

    🧹 chore: add a script for commit
    Add a script cm to use the commitizen for git commit when running npm run cm

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simple App' });
+  res.render('index', { title: 'Simple App again' });
 });
 
 module.exports = router;
